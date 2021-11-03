c7b6c18256f9dc3f02511d5a99795702be64c8b2
Nicolás Flores Montenegro <nicolas.fmontenegro@gmail.com>
2021-11-03 15:34:18 -0300

    update lsp utils

--- nvim/lua/lsp-config.lua
+++ nvim/lua/lsp-config.lua
@@ -153,11 +153,37 @@ require("lspconfig")["tsserver"].setup({
         client.resolved_capabilities.document_range_formatting = false
         local ts_utils = require("nvim-lsp-ts-utils")
         ts_utils.setup({
-            eslint_bin = "eslint_d",
-            eslint_enable_diagnostics = true,
+            debug = false,
+            disable_commands = false,
+            enable_import_on_completion = true,
+            import_on_completion_timeout = 5000,
+
+            -- eslint
             eslint_enable_code_actions = true,
+            eslint_bin = "eslint",
+            eslint_args = {"-f", "json", "--stdin", "--stdin-filename", "$FILENAME"},
+            eslint_enable_disable_comments = true,
+
+	          -- experimental settings!
+            -- eslint diagnostics
+            eslint_enable_diagnostics = true,
+            eslint_diagnostics_debounce = 250,
+
+            -- formatting
             enable_formatting = true,
             formatter = "prettier",
+            formatter_args = {"--stdin-filepath", "$FILENAME"},
+            format_on_save = true,
+            no_save_after_format = false,
+
+            -- parentheses completion
+            complete_parens = false,
+            signature_help_in_parens = true,
+
+            -- update imports on file move
+            update_imports_on_move = false,
+            require_confirmation_on_move = false,
+            watch_dir = "/src",
         })
         ts_utils.setup_client(client)
         on_attach(client, bufnr)
