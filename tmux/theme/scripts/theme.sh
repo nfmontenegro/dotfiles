8a81aea7e01f16570f1616127c03ce831f52b62f
Nicolas Camilo Flores Montenegro <nifloresm@falabella.cl>
2021-11-29 08:57:37 -0300

    add tmux themes

---
+++ tmux/theme/scripts/theme.sh
@@ -0,0 +1,77 @@
+#!/usr/bin/env bash
+
+get_tmux_option() {
+  local option=$1
+  local default_value=$2
+  local option_value=$(tmux show-option -gqv "$option")
+  if [ -z $option_value ]; then
+    echo $default_value
+  else
+    echo $option_value
+  fi
+}
+
+main()
+{
+  # set current directory variable
+  current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+  # Dracula Color Pallette
+  white='#f8f8f2'
+  gray='#44475a'
+  dark_gray='#282a36'
+  dark_purple='#633aa9'
+  cyan='#8be9fd'
+  orange='#ffb86c'
+  pink='#ff79c6'
+  yellow='#f1fa8c'
+
+  black="#000000"
+  red="#cd5c5c"
+  gray='#444444'
+  white='#ffffff'
+  dracula_purple="#b65fff"
+  dark_purple='#634aa9'
+  light_purple='#bd93f9'
+  plum="#ffafff"
+  fuchsia="#ff00ff"
+
+
+  theme_color=$(get_tmux_option "@theme-color" $dark_purple)
+  status_left_color=$(get_tmux_option "@status-left-color" $dracula_purple)
+  current_window_color=$(get_tmux_option "@current-window-color" $light_purple)
+  last_window_color=$(get_tmux_option "@last-window-color" $cyan)
+  zoom_window_color=$(get_tmux_option "@zoom-window-color" $red)
+
+
+  ## Status Bar
+  tmux set-option -g status-position bottom
+  tmux set-option -g status-justify left
+  tmux set-option -g status-bg "${gray}"
+  tmux set-option -g status-fg "${white}"
+
+  # Status left
+  tmux set-option -g status-left-length 50
+  tmux set-option -g status-left "#[bg=${theme_color}]#[fg=default] #S #[default]"
+
+  # Status right
+  tmux set-option -g status-right-length 100
+  tmux set-option -g status-right "#[bg=${theme_color}]#[fg=default] #(date '+%a %d %b %I:%M') #(../day_or_night.sh) "
+
+  # Window
+  tmux setw -g window-status-separator ""
+  tmux setw -g window-status-current-style "fg=${black},bg=${current_window_color}"
+  tmux setw -g window-status-current-format "#{?window_zoomed_flag,#[bg=${zoom_window_color}],#[fg=${black}]} #I:#W "
+  tmux setw -g window-status-format "#{?window_last_flag,#[fg=${last_window_color}]#[bold],#[fg=default]} #I:#W "
+
+  # Pane style
+  tmux set-option -g pane-active-border-style "fg=${light_purple}"
+  tmux set-option -g pane-border-style "fg=${gray}"
+
+  # Message styling
+  tmux set-option -g message-style "bg=${theme_color},fg=${white}"
+
+}
+
+# run main function
+main
